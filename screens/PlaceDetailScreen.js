3f400f65780c882e3fc04a9460c9c4d0ee0bc219
tglampedakis <theodore.glampedakis@intrasoft-intl.com>
2021-01-28 16:43:31 +0200

    React Native App: Places App - Initial Functionality (Camera - Internal Storage)

---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const PlaceDetailScreen = props => {
+    return (
+        <View>
+            <Text>Places Detail Screen.</Text>
+        </View>
+    );
+};
+
+PlaceDetailScreen.navigationOptions = navData => {
+    return {
+        headerTitle: navData.navigation.getParam('placeTitle')
+    };
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default PlaceDetailScreen;
